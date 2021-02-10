d3eb2cdae77be765e6151eedcc0d713e719b31ee
Triskae <me@filipedoutelsilva.com>
2021-02-10 22:31:32 +0100

    Login done

--- lib/auth/forms/login-form.dart
+++ lib/auth/forms/login-form.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class LoginForm extends StatefulWidget {
   @override
@@ -11,6 +12,8 @@ class LoginForm extends StatefulWidget {
 
 class LoginFormState extends State<LoginForm> {
   final _formKey = GlobalKey<FormState>();
+  String _email = "";
+  String _password = "";
 
   @override
   Widget build(BuildContext context) {
@@ -19,41 +22,53 @@ class LoginFormState extends State<LoginForm> {
         child: Column(
           children: <Widget>[
             TextFormField(
-              decoration:
-                  InputDecoration(hintText: "Email ou nom d'utilisateur"),
-              validator: (value) {
-                return emailCheck(value);
-              },
-            ),
+                decoration:
+                    InputDecoration(hintText: "Email ou nom d'utilisateur"),
+                keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  return emailCheck(value);
+                },
+                onSaved: (value) {
+                  setState(() {
+                    _email = value;
+                  });
+                }),
             TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(hintText: "Mot de passe"),
-              validator: (value) {
-                return passwordCheck(value);
-              },
-            ),
+                obscureText: true,
+                decoration: InputDecoration(hintText: "Mot de passe"),
+                keyboardType: TextInputType.visiblePassword,
+                validator: (value) {
+                  return passwordCheck(value);
+                },
+                onSaved: (value) {
+                  setState(() {
+                    _password = value;
+                  });
+                }),
             Padding(
               padding: EdgeInsets.only(top: 20),
-              child: ElevatedButton(
-                  onPressed: test,
-                  child: Text("Se connecter")),
+              child:
+                  ElevatedButton(onPressed: test, child: Text("Se connecter")),
             )
           ],
         ));
   }
 
   void test() async {
-    try {
-      UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-          email: "filipe.doutelsilva@hotmail.fr",
-          password: "Rammstein1998@"
-      );
-      print(userCredential.user.uid);
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        print('No user found for that email.');
-      } else if (e.code == 'wrong-password') {
-        print('Wrong password provided for that user.');
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(
+                email: _email,
+                password: _password);
+        print(userCredential.user.uid);
+      } on FirebaseAuthException catch (e) {
+        if (e.code == 'user-not-found') {
+          print('No user found for that email.');
+        } else if (e.code == 'wrong-password') {
+          print('Wrong password provided for that user.');
+        }
       }
     }
   }
@@ -62,7 +77,13 @@ class LoginFormState extends State<LoginForm> {
     if (value.isEmpty) {
       return "L'email est obligatoire";
     }
-    return null;
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value))
+      return 'Email non valide';
+    else
+      return null;
   }
 
   String passwordCheck(String value) {
